c67671717c947e72ea5f2b5fc8fd086d422ebebf
DAHEE <dahee@DAHEEui-MacBookPro-2.local>
2022-02-15 11:17:45 +0900

    add: router mapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import theme from './style/theme';
+import Home from './page/home';
+import Test1 from './page/test1';
+import Test2 from './page/test2';
+
+/*
+  1. css PC, Mobile 미디어 쿼리 styled-components로 빼기
+  2. onResize Hook으로 빼기 (PC, Mobile 일 때 텍스트 다르게 구현)
+  3. react-router-dom 기본
+    => 다른방식으로 구현해보기, ver6 적용, nested Router 있을땐 어떻게?
+*/
+
+interface Prop {
+  path: string;
+  component: () => JSX.Element;
 }
+
+const routes = [
+  {
+    path: '/',
+    component: <Home />
+  },
+  {
+    path: '/test1',
+    component: <Test1 />
+  },
+  {
+    path: '/test2',
+    component: <Test2 />
+  }
+];
+
+const App = () => {
+  return(
+    <ThemeProvider theme={theme}>
+      <Routes>
+        {
+          routes.map(({path, component}, i) => {
+            return(
+              <Route
+                path={path}
+                element={component}
+                key={path}
+              />
+            );
+          })
+        }
+      </Routes>
+    </ThemeProvider>
+  );
+};
+
+// nested routes 해결하기
+// const RouteWithSubRoutes = ({path, component}: any) => {
+//   return (
+//     <Route
+//       path={path}
+//       element={component}
+//     />
+//   );
+// };
 
 export default App;
